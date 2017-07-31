b3228ac978b6034450ca39fa815ca1a0cea435d1
Jonathan Huang <grokut@gmail.com>
2017-07-31 11:09:12 -0700

    remove "this"

--- src/create-data-graph.js
+++ src/create-data-graph.js
@@ -38,53 +38,62 @@ export default function createDataGraph(connector: Connector): DataGraph {
     parser: createParser()
   };
 
-  const children = [];
+  const childNodes = [];
 
   /**
    * An instance of a data graph. A data graph is basically a tree, where each
    * node represents a
    * @namespace Graph
    */
-  return {
-    /**
-     * Registers a custom expression or transform parser. The `typeDef` argument
-     * must be a valid type definition and the `typeParser` argument must be a
-     * function that matches the type of an ExpressionParser or TransformParser
-     * @see {@link https://github.com/mapd/mapd-data-layer/tree/master/src/parser/createParser.js|createParser.js}
-     * @memberof Graph
-     * @inner
-     */
-    registerParser(typeDef: TypeDefinition, typeParser: TypeParser): void {
-      context.parser.registerParser(typeDef, typeParser);
-    },
-
-    /**
-     * Returns all child data node instances of the graph.
-     * @memberof Graph
-     * @inner
-     */
-    children(): Array<DataNode> {
-      return children;
-    },
-
-    /**
-     * Creates a root data node instance. The source must be specific in the
-     * initial state. An example of a source, is a string pointing to a tables
-     * or an array of source transformations.
-     * @memberof Graph
-     * @inner
-     */
-    data(
-      state: string | Array<SourceTransform | DataState> | InitialDataNodeState
-    ): DataNode {
-      const dataNode = createDataNode(
-        context,
-        typeof state === "string" || Array.isArray(state)
-          ? { source: state, type: "root" }
-          : { ...state, type: "root" }
-      );
-      children.push(dataNode);
-      return dataNode;
-    }
+  const graphAPI = {
+    registerParser,
+    children,
+    data
   };
+
+  /**
+   * Registers a custom expression or transform parser. The `typeDef` argument
+   * must be a valid type definition and the `typeParser` argument must be a
+   * function that matches the type of an ExpressionParser or TransformParser
+   * @see {@link https://github.com/mapd/mapd-data-layer/tree/master/src/parser/createParser.js|createParser.js}
+   * @memberof Graph
+   * @inner
+   */
+  function registerParser(
+    typeDef: TypeDefinition,
+    typeParser: TypeParser
+  ): void {
+    context.parser.registerParser(typeDef, typeParser);
+  }
+
+  /**
+   * Returns all child data node instances of the graph.
+   * @memberof Graph
+   * @inner
+   */
+  function children(): Array<DataNode> {
+    return childNodes;
+  }
+
+  /**
+   * Creates a root data node instance. The source must be specific in the
+   * initial state. An example of a source, is a string pointing to a tables
+   * or an array of source transformations.
+   * @memberof Graph
+   * @inner
+   */
+  function data(
+    state: string | Array<SourceTransform | DataState> | InitialDataNodeState
+  ): DataNode {
+    const dataNode = createDataNode(
+      context,
+      typeof state === "string" || Array.isArray(state)
+        ? { source: state, type: "root" }
+        : { ...state, type: "root" }
+    );
+    childNodes.push(dataNode);
+    return dataNode;
+  }
+
+  return graphAPI;
 }
--- src/create-data-node.js
+++ src/create-data-node.js
@@ -58,60 +58,71 @@ export default function createDataNode(
    * @namespace Data
    * @see {@link https://github.com/mapd/mapd-data-layer/tree/master/src/create-data-node.js|create-data-node.js}
    */
-  return {
-    /**
-     * Returns the state of the data node.
-     * @memberof Data
-     * @inner
-     */
-    getState(): DataState {
-      return state;
-    },
-
-    /**
-     * Sets the transform state of the data node. Either takes in an array of
-     * transforms or a function that takes and returns an array of transforms
-     * @memberof Data
-     * @inner
-     */
-    transform(setter: SetTransform): DataNode {
-      state.transform = typeof setter === "function"
-        ? setter(state.transform)
-        : setter;
-      return this;
-    },
-
-    /**
-     * Returns the SQL string representation of the set of transforms from
-     * the node instance to its source root in the graph
-     * @memberof Data
-     * @inner
-     */
-    toSQL(): string {
-      return context.parser.write(reduceToSQL(context, this));
-    },
-
-    /**
-     * Uses the `connector` in the graph context to execute data node's
-     * SQL query representation and returns queried data as a promise.
-     * @memberof Data
-     * @inner
-     */
-    values() {
-      return context.connector.query(
-        context.parser.write(reduceToSQL(context, this))
-      );
-    },
-
-    /**
-     * Creates a data node instance and sets it as a child of the parent.
-     * @memberof Data
-     * @inner
-     */
-    data(childState?: InitialDataNodeState): DataNode {
-      const dataNode = createDataNode(context, { ...childState, source: this });
-      state.children.push(dataNode);
-      return dataNode;
-    }
+  const dataNodeAPI = {
+    getState,
+    transform,
+    toSQL,
+    values,
+    data
   };
+
+  /**
+   * Returns the state of the data node.
+   * @memberof Data
+   * @inner
+   */
+  function getState(): DataState {
+    return state;
+  }
+
+  /**
+   * Sets the transform state of the data node. Either takes in an array of
+   * transforms or a function that takes and returns an array of transforms
+   * @memberof Data
+   * @inner
+   */
+  function transform(setter: SetTransform): DataNode {
+    state.transform = typeof setter === "function"
+      ? setter(state.transform)
+      : setter;
+    return dataNodeAPI;
+  }
+
+  /**
+   * Returns the SQL string representation of the set of transforms from
+   * the node instance to its source root in the graph
+   * @memberof Data
+   * @inner
+   */
+  function toSQL(): string {
+    return context.parser.write(reduceToSQL(context, dataNodeAPI));
+  }
+
+  /**
+   * Uses the `connector` in the graph context to execute data node's
+   * SQL query representation and returns queried data as a promise.
+   * @memberof Data
+   * @inner
+   */
+  function values() {
+    return context.connector.query(
+      context.parser.write(reduceToSQL(context, dataNodeAPI))
+    );
+  }
+
+  /**
+   * Creates a data node instance and sets it as a child of the parent.
+   * @memberof Data
+   * @inner
+   */
+  function data(childState?: InitialDataNodeState): DataNode {
+    const dataNode = createDataNode(context, {
+      ...childState,
+      source: dataNodeAPI
+    });
+    state.children.push(dataNode);
+    return dataNode;
+  }
+
+  return dataNodeAPI;
 }
