0b63281f347f7dbeaaa7d840e3ea28b94c756576
Vraj Pandya <vrajspandya@gmail.com>
2018-12-21 11:36:26 -0800

    use smart modular multiplication arithmetic to reduce chance of int overflow on the backend.

--- packages/data-layer/src/parser/parse-sample.js
+++ packages/data-layer/src/parser/parse-sample.js
@@ -1,7 +1,7 @@
 // @flow
 import type { SQL } from "./write-sql";
 
-const GOLDEN_RATIO = 265445761;
+const GOLDEN_RATIO = 2654435761;
 const THIRTY_TWO_BITS = 4294967296;
 
 export default function sample(sql: SQL, transform: Sample): SQL {
@@ -11,8 +11,13 @@ export default function sample(sql: SQL, transform: Sample): SQL {
     const ratio = Math.min(limit / size, 1.0);
     if (ratio < 1) {
       const threshold = Math.floor(THIRTY_TWO_BITS * ratio);
+      // We are using simple modulo arithmetic expression conversion, 
+      // (A * B) mod C = (A mod C * B mod C) mod C, 
+      // to optimize the filter here. This helps  the overflow on the backend.
+      // We don't have the full modulo expression for golden ratio since 
+      // that is a constant expression and we can avoid that execution
       sql.where.push(
-        `MOD(${sql.from}.rowid * ${GOLDEN_RATIO}, ${THIRTY_TWO_BITS}) < ${threshold}`
+        `MOD( MOD (${sql.from}.rowid, ${THIRTY_TWO_BITS}) * ${GOLDEN_RATIO} , ${THIRTY_TWO_BITS}) < ${threshold}`
       );
     }
   }
