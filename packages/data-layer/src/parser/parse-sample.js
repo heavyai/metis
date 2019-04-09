719d614703fe6de8fe1ef01d46f220f4121e5a6c
uyanga-gb <uyanga.gb@gmail.com>
2019-04-09 14:36:29 -0700

    use thirty one bit in sampling MOD

--- packages/data-layer/src/parser/parse-sample.js
+++ packages/data-layer/src/parser/parse-sample.js
@@ -2,6 +2,7 @@
 import type { SQL } from "./write-sql";
 
 const GOLDEN_RATIO = 2654435761;
+const THIRTY_ONE_BITS = 2147483648;
 const THIRTY_TWO_BITS = 4294967296;
 
 export default function sample(sql: SQL, transform: Sample): SQL {
@@ -11,7 +12,7 @@ export default function sample(sql: SQL, transform: Sample): SQL {
 
   if (transform.method === "multiplicativeRowid" && ratio < 1) {
     sql.where.push(
-      `((MOD( MOD (${transform.field}, ${THIRTY_TWO_BITS}) * ${GOLDEN_RATIO} , ${THIRTY_TWO_BITS}) < ${threshold}) OR (${transform.field} IN (${transform.expr.join(", ")})))`
+      `((MOD( MOD (${transform.field}, ${THIRTY_ONE_BITS}) * ${GOLDEN_RATIO} , ${THIRTY_TWO_BITS}) < ${threshold}) OR (${transform.field} IN (${transform.expr.join(", ")})))`
     );
   } else if (transform.method === "multiplicative" && ratio < 1) {
     // We are using simple modulo arithmetic expression conversion, 
@@ -20,7 +21,7 @@ export default function sample(sql: SQL, transform: Sample): SQL {
     // We don't have the full modulo expression for golden ratio since 
     // that is a constant expression and we can avoid that execution
     sql.where.push(
-      `MOD( MOD (${sql.from}.rowid, ${THIRTY_TWO_BITS}) * ${GOLDEN_RATIO} , ${THIRTY_TWO_BITS}) < ${threshold}`
+      `MOD( MOD (${sql.from}.rowid, ${THIRTY_ONE_BITS}) * ${GOLDEN_RATIO} , ${THIRTY_TWO_BITS}) < ${threshold}`
     );
   }
 
