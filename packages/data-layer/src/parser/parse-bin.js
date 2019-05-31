f2709d9afde65a4032a71da8f7b63cd58e133ffe
Jonathan Rajavuori <jrajav@gmail.com>
2019-05-31 09:32:21 -0500

    Add epsilon extents to binning calculation

--- packages/data-layer/src/parser/parse-bin.js
+++ packages/data-layer/src/parser/parse-bin.js
@@ -1,16 +1,28 @@
 // @flow
 import type { SQL } from "./write-sql";
 
+// This value is just about the smallest that can be meaningfully represented by a
+// 32-bit float, and provides a bound around possible roundoff errors
+const FLOAT_EPSILON = Math.pow(2, -23)
+
 export default function parseBin(
   sql: SQL,
-  { field, as, extent, maxbins }: Bin
+  { field, as, extent: [start, end], maxbins }: Bin
 ): SQL {
+  // Including FLOAT_EPSILON in these calculations effectively adds a tiny extra range
+  // FLOAT_EPSILON wide on the start and end of the extent, for two reasons:
+  // 1) Account for possible roundoff errors that occurred when retrieving them initially, and
+  // 2) Include the maximum value that is exactly `end`, where it would otherwise fall off the top bin
+  const inputRange = end - start + 2 * FLOAT_EPSILON
+  const binRange = maxbins - FLOAT_EPSILON
+  const stretchFactor = binRange / inputRange
+  const shiftAmount = -(FLOAT_EPSILON * binRange) / inputRange
+
   sql.select.push(
-    `cast((cast(${field} as float) - ${extent[0]}) * ${maxbins / (extent[1] - extent[0])} as int) as ${as}`
+    `cast((cast(${field} as float) - ${start}) * ${stretchFactor} + ${shiftAmount} as int) as ${as}`
   );
   sql.where.push(
-    `((${field} >= ${extent[0]} AND ${field} <= ${extent[1]}) OR (${field} IS NULL))`
+    `((${field} >= ${start - FLOAT_EPSILON} AND ${field} <= ${end + FLOAT_EPSILON}) OR (${field} IS NULL))`
   );
-  sql.having.push(`(${as} >= 0 AND ${as} < ${maxbins} OR ${as} IS NULL)`);
   return sql;
 }
