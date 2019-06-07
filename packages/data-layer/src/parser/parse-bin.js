cc4bfe4d7f106649fc2d2041c195744e3ee128e0
Jim Thomason <jim@japanesewartuba.com>
2019-06-07 14:34:42 -0500

    jimt/FE-8667 - fidged variables; added comments

--- packages/data-layer/src/parser/parse-bin.js
+++ packages/data-layer/src/parser/parse-bin.js
@@ -5,16 +5,26 @@ export default function parseBin(
   sql: SQL,
   { field, as, extent, maxbins }: Bin
 ): SQL {
-  const binRange = extent[1] - extent[0];
-  
+
+  // numBins is used conditionally in our query building below.
+  // first of all, if we're going to fall into the overflow bin AND we have
+  // 0 bins, then we should land in bin 0. Otherwise, we should land in the last
+  // bin.
+  //
+  // later, we calculate the binning magic number based on numBins - dividing either
+  // by it or 1 if it doesn't exist, to prevent a divide by zero / infinity error.
+  //
+  // The logic used by mapd-crossfilter's getBinnedDimExpression is completely different.
+  const numBins  = extent[1] - extent[0];
+
   sql.select.push(
     `case when
       ${field} >= ${extent[1]}
     then
-      ${ binRange === 0 ? 0 : maxbins - 1}
+      ${ numBins === 0 ? 0 : maxbins - 1}
     else
       cast((cast(${field} as float) - ${extent[0]}) * ${maxbins /
-      (binRange || 1) } as int)
+      (numBins || 1) } as int)
     end
     as ${as}`
   );
